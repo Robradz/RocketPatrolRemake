581a60b0a93d32ffd55ebf2ae4438d7965c9a98b
Robert Radzville <rradzvil@ucsc.edu>
2021-04-09 14:25:35 -0700

    After Class Friday 4/9 - keyF not defined error

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,13 +4,43 @@ class Play extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('starfield', '../assets/starfield.png');
+        this.load.image('starfield', 'assets/starfield.png');
+        this.load.image('rocket', 'assets/rocket.png');
+        this.load.image('ship', 'assets/spaceship.png');
     }
 
     create() {
         // Show Starfield
         this.starfield = this.add.tileSprite(0, 0, 640, 480, 
             'starfield').setOrigin(0,0);
+
+        this.p1Rocket = new Rocket(
+            this, 
+            game.config.width / 2,
+            game.config.height - borderUISize - borderPadding,
+            'rocket'
+        );
+
+        this.ship1 = new Ship(
+            this,
+            100,
+            200,
+            'ship'
+        );
+
+        this.ship2 = new Ship(
+            this,
+            300,
+            240,
+            'ship'
+        );
+
+        this.ship3 = new Ship(
+            this,
+            380,
+            300,
+            'ship'
+        );
 
         // Green UI background
         this.add.rectangle(
@@ -26,10 +56,35 @@ class Play extends Phaser.Scene {
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
+    
+        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+
     }
 
     update() {
         this.starfield.tilePositionX -= 4;
+        this.p1Rocket.update();
+
+        this.ship1.update();
+        this.ship2.update();
+        this.ship3.update();
+
+        this.checkCollision(this.p1Rocket, this.ship1);
+        this.checkCollision(this.p1Rocket, this.ship2);
+        this.checkCollision(this.p1Rocket, this.ship3);
+    }
+
+    checkCollision(rocket, ship) {
+        if (rocket.x > ship.x && 
+            rocket.x < shipt.x + ship.width && 
+            rocket.y > ship.y && 
+            rocket.y < ship.y + ship.height) {
+                ship.reset();
+                rocket.reset();
+        }
     }
 
 }
